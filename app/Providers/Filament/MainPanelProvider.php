fe8e17334adb97b8b181652767b492537e54f509
Janyk <me@janyk.dev>
2024-12-11 19:56:32 +0100

    feat: add brand logo and update primary color in panel

--- app/Providers/Filament/MainPanelProvider.php
+++ app/Providers/Filament/MainPanelProvider.php
@@ -30,9 +30,11 @@ class MainPanelProvider extends PanelProvider
             ->default()
             ->id('main')
             ->path('')
+            ->brandLogo(fn () => asset('logo.svg'))
+            ->brandLogoHeight('3rem')
             ->login()
             ->colors([
-                'primary' => Color::Amber,
+                'primary' => Color::Red,
             ])
             ->discoverResources(in: app_path('Filament/Resources'), for: 'App\\Filament\\Resources')
             ->discoverPages(in: app_path('Filament/Pages'), for: 'App\\Filament\\Pages')
