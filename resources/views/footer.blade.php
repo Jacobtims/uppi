1605feae76a58a831581857a8ae8e4db1fb7bfa7
Janyk <me@janyk.dev>
2024-12-16 21:29:13 +0100

    style(footer): update padding for footer elements

--- resources/views/footer.blade.php
+++ resources/views/footer.blade.php
@@ -1,11 +1,11 @@
-<footer class="py-6">
+<footer class="py-6 px-1">
 <div class="flex justify-center items-center">
     <a href="https://www.webmethod.nl/?utm_source=uppi&utm_medium=footer">
         <img src="https://www.webmethod.nl/assets/images/logo/logo.png" alt="Webmethod" style="width: 100px; opacity: 0.4;">
     </a>
 </div>
 
-<div class="flex justify-center items-center py-6">
+<div class="flex justify-center items-center py-6 hidden sm:block">
     <p class="text-xs text-gray-400">
         Uppi is a free and open source project built by <a href="https://www.janyksteenbeek.nl/?utm_source=uppi&utm_medium=footer" class="text-gray-400 font-medium hover:text-gray-700">Janyk Steenbeek</a>. Consider <a href="https://github.com/sponsors/janyksteenbeek" class="text-gray-400 font-semibold hover:text-gray-700">sponsoring</a> or or <a href="https://github.com/janyksteenbeek/uppi" class="text-gray-400 font-medium hover:text-gray-700">contributing</a>.
     </p>
