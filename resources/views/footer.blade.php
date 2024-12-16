c47b04aace6ee0a7e88aac2475c301a9e856674c
Janyk <me@janyk.dev>
2024-12-16 21:31:58 +0100

    style(footer): Improve footer layout using flexbox

--- resources/views/footer.blade.php
+++ resources/views/footer.blade.php
@@ -5,10 +5,8 @@
     </a>
 </div>
 
-<div class="flex justify-center items-center py-6 hidden sm:block">
-    <p class="text-xs text-gray-400">
-        Uppi is a free and open source project built by <a href="https://www.janyksteenbeek.nl/?utm_source=uppi&utm_medium=footer" class="text-gray-400 font-medium hover:text-gray-700">Janyk Steenbeek</a>. Consider <a href="https://github.com/sponsors/janyksteenbeek" class="text-gray-400 font-semibold hover:text-gray-700">sponsoring</a> or <a href="https://github.com/janyksteenbeek/uppi" class="text-gray-400 font-medium hover:text-gray-700">contributing</a>.
-    </p>
+<div class="justify-center items-center py-6 hidden sm:flex text-xs text-gray-400">
+    Uppi is a free and open source project built by <a href="https://www.janyksteenbeek.nl/?utm_source=uppi&utm_medium=footer" class="text-gray-400 font-medium hover:text-gray-700">Janyk Steenbeek</a>. Consider <a href="https://github.com/sponsors/janyksteenbeek" class="text-gray-400 font-semibold hover:text-gray-700">sponsoring</a> or <a href="https://github.com/janyksteenbeek/uppi" class="text-gray-400 font-medium hover:text-gray-700">contributing</a>.
 </div>
 </footer>
 
