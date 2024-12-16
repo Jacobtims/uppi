f49e4d182523e9157d183a27a8ceb37d285b3b6e
Janyk <me@janyk.dev>
2024-12-16 15:39:26 +0100

    chore: move tippy.js CSS import to app.js

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,5 +1,6 @@
 import './bootstrap';
 import tippy from 'tippy.js';
+import 'tippy.js/dist/tippy.css';
 
 document.addEventListener('livewire:initialized', () => {
     tippy('[data-tippy-content]', {
