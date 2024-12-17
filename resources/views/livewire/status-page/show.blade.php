35cc0ddf3a75f41a69e779233b185a94578c2536
Janyk <me@janyk.dev>
2024-12-17 21:21:34 +0100

    chore: remove dark classes

--- resources/views/livewire/status-page/show.blade.php
+++ resources/views/livewire/status-page/show.blade.php
@@ -1,11 +1,11 @@
 <div>
     <div class="max-w-2xl mx-auto my-4 px-2">
-        <div class="bg-white dark:bg-gray-800 shadow rounded-lg border border-neutral-100 dark:border-gray-700 p-4">
+        <div class="bg-white shadow rounded-lg border border-neutral-100 p-4">
             <div class="flex items-center justify-between">
                 @if($statusPage->logo_url)
                     <img src="{{ Storage::url($statusPage->logo_url) }}" alt="{{ $statusPage->name }}" class="h-8">
                 @else
-                    <h1 class="text-xl font-bold dark:text-white">{{ $statusPage->name }}</h1>
+                    <h1 class="text-xl font-bold">{{ $statusPage->name }}</h1>
                 @endif
             </div>
         </div>
