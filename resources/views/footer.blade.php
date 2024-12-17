1ea441024b697ac1d283379f7f9f88cd73819087
Janyk <me@janyk.dev>
2024-12-17 19:53:41 +0100

    style(footer): fix whitespace in footer attribution link

--- resources/views/footer.blade.php
+++ resources/views/footer.blade.php
@@ -6,7 +6,7 @@
 </div>
 
 <div class="justify-center items-center py-6 hidden sm:flex text-xs text-gray-400">
-    Uppi is a free and open source project built by <a href="https://www.janyksteenbeek.nl/?utm_source=uppi&utm_medium=footer" class="text-gray-400 font-medium hover:text-gray-700">Janyk Steenbeek</a>.
+    Uppi is a free and open source project built by <a href="https://www.janyksteenbeek.nl/?utm_source=uppi&utm_medium=footer" class="text-gray-400 font-medium hover:text-gray-700"> Janyk Steenbeek</a>.
     Consider <a href="https://github.com/sponsors/janyksteenbeek" class="text-gray-400 font-semibold hover:text-gray-700"> sponsoring </a> or <a href="https://github.com/janyksteenbeek/uppi" class="text-gray-400 font-medium hover:text-gray-700"> contributing</a>.
 </div>
 </footer>
