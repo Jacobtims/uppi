6c01fe26ca1a7810d4a10531cf5d50b7a3ddba5f
Janyk <me@janyk.dev>
2024-12-22 12:31:38 +0100

    style: update SVG icon in app info component

--- resources/views/filament/forms/components/uppi-app-info.blade.php
+++ resources/views/filament/forms/components/uppi-app-info.blade.php
@@ -32,7 +32,10 @@
             After installing the app, generate a connection code to connect it to your account.
         </p>
         <a href="{{ $personal_access_tokens_url }}" class="inline-flex items-center justify-center px-4 py-2 bg-primary-600 text-white rounded-lg hover:bg-primary-500 gap-2">
-            <x-heroicon-m-key class="w-5 h-5 mr-2" />
+            <svg class="w-5 h-5 mr-2" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+            <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 5.25a3 3 0 0 1 3 3m3 0a6 6 0 0 1-7.029 5.912c-.563-.097-1.159.026-1.563.43L10.5 17.25H8.25v2.25H6v2.25H2.25v-2.818c0-.597.237-1.17.659-1.591l6.499-6.499c.404-.404.527-1 .43-1.563A6 6 0 1 1 21.75 8.25Z" />
+            </svg>
+
             <span>Connections</span>
         </a>
     </div>
