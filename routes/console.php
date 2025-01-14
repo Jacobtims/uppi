826dcd799e96c7bc7f5969e564c32175317ba527
Janyk <me@janyk.dev>
2025-01-14 21:55:34 +0100

    feat(routes): add horizon snapshot command to schedule

--- routes/console.php
+++ routes/console.php
@@ -5,3 +5,4 @@ use Illuminate\Support\Facades\Schedule;
 Schedule::command('monitors:check')->everyMinute();
 Schedule::command('cache:refresh-tasks')->everyMinute();
 Schedule::command('checks:cleanup')->dailyAt('03:00');
+Schedule::command('horizon:snapshot')->everyFiveMinutes();
